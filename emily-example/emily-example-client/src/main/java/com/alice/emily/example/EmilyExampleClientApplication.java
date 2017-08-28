dd4e7140f41c7976643c05c2a795ed42070bb9a0
lianhao0310 <lianhao0310@163.com>
2017-08-28 20:36:50 +0800

    update

---
+++ emily-example/emily-example-client/src/main/java/com/alice/emily/example/EmilyExampleClientApplication.java
@@ -0,0 +1,12 @@
+package com.alice.emily.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilyExampleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmilyExampleClientApplication.class, args);
+	}
+}
