5cf9c0025b0bae04d2ef518edafd69aeda8fbe3f
lianhao0310 <lianhao0310@163.com>
2018-12-27 21:05:44 +0800

    update

--- emily-test/src/test/java/com/alice/emily/utils/LazyTest.java
+++ emily-test/src/test/java/com/alice/emily/utils/LazyTest.java
@@ -7,20 +7,23 @@ import org.junit.Test;
  */
 public class LazyTest {
     private Lazy<Person> stringLazy = Lazy.of(Person::new);
+
     @Test
-    public void testGet(){
+    public void testGet() {
         System.out.println(stringLazy); //com.alice.emily.utils.Lazy@2a2d45ba
         System.out.println(stringLazy.get()); //mike lian
     }
-    public class Person{
+
+    public class Person {
         private String firstName;
         private String lastName;
-        public Person(){
+
+        public Person() {
             this.firstName = "mike";
             this.lastName = "lian";
         }
 
-        public String toString(){
+        public String toString() {
             return this.firstName + " " + this.lastName;
         }
     }
